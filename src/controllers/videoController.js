d1d0cfe57e8e66c9c513c49347f2fa8b234e8175
MrB4N4N4 <2016rudgns@naver.com>
2021-05-31 20:41:25 +0900

    Pug is Amazing

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,18 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title:"First Video",
+      rating:5,
+      comments:2,
+      created: "12"
+    }
+  ];
+  return res.render("home", {pageTitle: "Home", videos});
+};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  return res.send("Edit");
-};
 export const deleteVideo = (req, res) => {
   return res.send("Delete Video");
 };
