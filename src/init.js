b9126990084e585720f063c44af6823a74b30dfa
MrB4N4N4 <2016rudgns@naver.com>
2021-06-02 17:29:17 +0900

    Initial mongoDb settings

---
+++ src/init.js
@@ -0,0 +1,11 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4000;
+
+//server starts to listen PORT 4000
+const handleListening = () =>
+  console.log(`▶ Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
+
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 console.log(process.cwd())
 
@@ -19,8 +18,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-//server starts to listen PORT 4000
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+export default app;
