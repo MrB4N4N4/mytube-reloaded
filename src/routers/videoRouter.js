cb7c380ef82cd2e28a4ed59897e713941a517b4c
MrB4N4N4 <2016rudgns@naver.com>
2021-06-08 22:28:15 +0900

    complete CRUD of video controller

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
     watch,
-    upload,
     deleteVideo,
     getEdit,
     postEdit,
@@ -13,6 +12,8 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-z]{24})", watch);
 videoRouter.route("/:id([0-9a-z]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-z]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
+
 
 export default videoRouter;
