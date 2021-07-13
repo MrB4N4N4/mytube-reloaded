4e61a71ff7ce5e8b8d89e438b38f997c776700a1
MrB4N4N4 <2016rudgns@naver.com>
2021-07-13 12:06:36 +0900

    Edit DB_URL

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
@@ -12,5 +12,5 @@ const db = mongoose.connection;
 const handleOpen = () => console.log("✅ Connected to DB");
 const handleError = (error) => console.log("❗ DB Error", error);
 
-db.on("error", handleError); 
+db.on("error", handleError);
 db.once("open", handleOpen);
